63992c5255a05f841bae6e39842123b65ebee377
vargape <peter.varga@drei.com>
2017-05-26 13:56:45 +0200

    Before Maven conversion

--- AutoUnit/src/autounit/popup/actions/CreatenewUnitTestClassAction.java
+++ AutoUnit/src/autounit/popup/actions/CreatenewUnitTestClassAction.java
@@ -1,44 +1,43 @@
 package autounit.popup.actions;
 
 import org.eclipse.jface.action.IAction;
-import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.IActionDelegate;
 import org.eclipse.ui.IObjectActionDelegate;
 import org.eclipse.ui.IWorkbenchPart;
 
 public class CreatenewUnitTestClassAction implements IObjectActionDelegate {
 
-	private Shell shell;
-	
-	/**
-	 * Constructor for Action1.
-	 */
-	public CreatenewUnitTestClassAction() {
-		super();
-	}
+    
+    
+    private Shell shell;
 
-	/**
-	 * @see IObjectActionDelegate#setActivePart(IAction, IWorkbenchPart)
-	 */
-	public void setActivePart(IAction action, IWorkbenchPart targetPart) {
-		shell = targetPart.getSite().getShell();
-	}
+    /**
+     * Constructor for Action1.
+     */
+    public CreatenewUnitTestClassAction() {
+	super();
+    }
 
-	/**
-	 * @see IActionDelegate#run(IAction)
-	 */
-	public void run(IAction action) {
-		MessageDialog.openInformation(
-			shell,
-			"AutoUnit",
-			"New Action was executed.");
-	}
+    /**
+     * @see IObjectActionDelegate#setActivePart(IAction, IWorkbenchPart)
+     */
+    public void setActivePart(IAction action, IWorkbenchPart targetPart) {
+	shell = targetPart.getSite().getShell();
+    }
 
-	/**
-	 * @see IActionDelegate#selectionChanged(IAction, ISelection)
-	 */
-	public void selectionChanged(IAction action, ISelection selection) {
-	}
+    /**
+     * @see IActionDelegate#run(IAction)
+     */
+    public void run(IAction action) {
+
+    }
+
+    /**
+     * @see IActionDelegate#selectionChanged(IAction, ISelection)
+     */
+    public void selectionChanged(IAction action, ISelection selection) {
+    }
 
 }
